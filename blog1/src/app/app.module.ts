799134d2dcf4703c5903e70c42cd6a2d85123993
Justyna Siedlarz <tysia515@gmail.com>
2018-10-26 12:15:57 +0200

    lab5

---
+++ blog1/src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Routes,RouterModule} from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogItemComponent } from './components/blog-item/blog-item.component';
+import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
+import { BlogItemTextPipe } from './blog-item-text.pipe';
+import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
+const appRoutes: Routes = [
+  {
+	path: 'home-component',
+	component: HomeComponentComponent
+  },
+  {
+	path: 'blog',
+	component: BlogComponent,
+},
+{
+	path: 'quiz',
+	component: QuizComponent ,
+},
+{
+	path: 'contact',
+	component: ContactComponent,
+}
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    QuizComponent,
+    HomeComponentComponent,
+    NavbarComponent,
+    BlogComponent,
+    BlogItemComponent,
+    BlogItemTextComponent,
+    BlogItemTextPipe,
+    BlogItemImageComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
+
+
